a108b32476a6d74a21fd7c2bfaa228055bb68671
omar-hekal <137761235+omar-hekal@users.noreply.github.com>
2025-05-04 01:47:00 +0300

    add SoliderReID adaptor module

---
+++ external/adaptors/soliderreid_adaptor.py
@@ -0,0 +1,23 @@
+import torch
+
+from solider_reid.config import cfg
+from solider_reid.model import make_model
+
+
+class SoliderReID(torch.nn.Module):
+    def __init__(self, weights_path):
+        super().__init__()
+        config_file = "external/solider_reid/configs/iust/swin_small.yml"
+        cfg.merge_from_file(config_file)
+        cfg.TEST.WEIGHT = weights_path
+        self.model = make_model(cfg, num_class=0, camera_num=1, view_num = 1,)
+        self.model.eval()
+        self.model.cuda()
+
+        self.model.load_param(cfg.TEST.WEIGHT)
+
+    def forward(self, batch):
+        # Uses half during training
+        # batch = batch.half()
+        with torch.no_grad():
+            return self.model(batch)
